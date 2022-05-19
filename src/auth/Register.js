1ab8072de295b60c995a4a08c602ccd5e8f65481
amichalo421 <andre.michalowski@gmail.com>
2022-05-19 13:15:35 -0400

    Basic registration form

--- src/auth/Register.js
+++ src/auth/Register.js
@@ -5,13 +5,46 @@ const Register = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleSubmit = () => {
-    alert("send user info to backend");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.table({ name, email, password });
   };
 
   const registerForm = () => (
-    <form onSubmit={handleSubmit}>
-      show the form <button type="submit">Submit</button>{" "}
+    <form onSubmit={handleSubmit} className="mt-3">
+      <div className="form-group mb-3">
+        <label className="form-label">Name:</label>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Enter name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div className="form-group mb-3">
+        <label className="form-label">Email:</label>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="emailgoeshere@email.com"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div className="form-group mb-3">
+        <label className="form-label">Password:</label>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Enter password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div className="form-group">
+        <button className="btn btn-primary form-control">Submit</button>
+      </div>
     </form>
   );
 
